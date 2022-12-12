7abba6c6401e1306fb236f6cc78914a392936bb5
yeonzzoo <a1urem2nt@gmail.com>
2022-12-12 21:29:59 +0900

    third

--- src/main/java/com/codestates/ToDoApp/ToDoAppApplication.java
+++ src/main/java/com/codestates/ToDoApp/ToDoAppApplication.java
@@ -2,12 +2,18 @@ package com.codestates.ToDoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { // (2)
+		return builder.sources(ToDoAppApplication.class);
 	}
 
 }
